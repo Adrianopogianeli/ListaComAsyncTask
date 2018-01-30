16a903b07be5916779516770f47d1fb89bab7d2a
logonrm <logonrm@fiap.com.br>
2018-01-29 23:12:32 -0200

    Inicio do projeto

---
+++ app/src/main/java/br/com/adriano/listacomasynctask/listener/OnItemClickListener.java
@@ -0,0 +1,13 @@
+package br.com.adriano.listacomasynctask.listener;
+
+import android.view.View;
+
+/**
+ * Created by logonrm on 29/01/2018.
+ */
+
+public interface OnItemClickListener {
+
+    void onClick(View view, int position);
+
+}
